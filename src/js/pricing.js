406653a7313fe2a33eef7b46e13797121ec55cdc
das41k <armenpetrosan319@gmail.com>
2024-08-07 16:18:42 +0300

    Pricing section plans

---
+++ src/js/pricing.js
@@ -0,0 +1,7 @@
+import '../assets/scss/pricing.scss';
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
+useTheme();
+useBurger();
+usePriceSwitcher();
